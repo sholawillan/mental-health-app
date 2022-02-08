92a11fb4bcfb7bdc5c196520478cac4840b00011
Shola Willan <sholawillan3@gmail.com>
2022-02-08 15:08:45 +0000

    init commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+    <title>My mental health app</title>
+    <link rel="stylesheet" href="/app.css">
+
+    
+    
+    <body>
+      <artical>
+        <?= $post; ?>
+        
+    </artical>
+
+  <a href="/">Go Back</a>
+
+</body> 
+        
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+    <!doctype html>
+
+    <title>My mental health app</title>
+    <link rel="stylesheet" href="/app.css">
+
+    
+    
+    <body>
+        <artical>
+            <h1><a href="/posts/my-first-post">Mindfulness</a></h1>  
+            
+            <p>
+            Find out a bit about what Mindfulness is  
+            </p>
+            </artical>
+        
+            <h1><a href="/posts/my-second-post">My second post</a></h1>
+            <p>
+              Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make healthy choices. 1. Mental health is important at every stage of life, from childhood and adolescence through adulthood.   
+            </p>
+        </artical>  
+     <body> 
+        
